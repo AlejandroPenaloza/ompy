8e4c5d954a6b9d7a5628fbd0c2115516cacd3153
Alejandro Penaloza <alejandro.penaloza.20@gmail.com>
2021-03-04 19:44:17 -0600

    ompy.py 0304 1944 commit

--- ompy.py
+++ ompy.py
@@ -1,22 +1,23 @@
-# coding=utf-8
 import math
 import re
 
 
 def to_dec_degrees(
-    theta, 
-    from_radians=True, 
-    from_degs_mins_secs=False, 
-    from_gradians=False, 
-    from_turns=False):
-  
-    """It takes an angle given in the following units: degrees-minutes-seconds, radians, gradians and turns;
+        theta,
+        from_radians=True,
+        from_degs_mins_secs=False,
+        from_gradians=False,
+        from_turns=False
+        ):
+    """
+    It takes an angle given in the following units: degrees-minutes-seconds, radians, gradians and turns;
     and returns the angle converted in decimal degrees.
     Angles must be provided as 'int' or 'float', denoting the magnitude with no unit,
-    except for degrees-minutes-seconds, which is to be 'str' pattern DdMM'SS'' or D°MM'SS''."""
+    except for degrees-minutes-seconds, which is to be 'str' pattern DdMM'SS'' or D°MM'SS''.
+    """
 
     arguments_False_by_default = list(locals().values())[-3:]
-    
+
     if any(arguments_False_by_default):
 
         try:
@@ -25,19 +26,20 @@ def to_dec_degrees(
         except AssertionError:
             raise TypeError(
                 "Unit error; more than one unit requested for converting.\nOnly one supported.")
-    
+
     if from_degs_mins_secs:
 
         try:
-            assert type(theta) == str and re.fullmatch("[0-9]+[d"+str(chr(176))+"][0-9]{2}'[0-9]{2}.*[0-9]*''", theta)
+            assert type(theta) == str and re.fullmatch("[0-9]+[d" + str(chr(176)) + "][0-9]{2}'[0-9]{2}.*[0-9]*''",
+                                                       theta)
 
         except AssertionError:
             raise TypeError(
-                "Angle ", theta, 
+                "Angle ", theta,
                 " class type not supported; not matching correct format.",
-                "Requested numeric type and degrees, minutes, seconds pattern(D", 
+                "Requested numeric type and degrees, minutes, seconds pattern(D",
                 str(chr(176)), "MM'SS'') or (DdMM'SS''")
-            
+
         if str(chr(176)) in theta:
             separator = str(chr(176))
 
@@ -48,4 +50,4 @@ def to_dec_degrees(
         degrees = float(degs_mins_secs[0])
         minutes = float(degs_mins_secs[1].split("'")[0])
         seconds = float(degs_mins_secs[1].split("'")[1])
-        return round(degrees + (minutes/60) + (seconds/3600), 15)
+        return round(degrees + (minutes / 60) + (seconds / 3600), 15)
