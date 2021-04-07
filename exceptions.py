3d78b0aee642c52f2663a924c9c727238f21a7bf
Alejandro Penaloza <alejandro.penaloza.20@gmail.com>
2021-04-06 21:09:08 -0500

    exceptions.py first inclusion

---
+++ exceptions.py
@@ -0,0 +1,15 @@
+class UnitError(Exception):
+    
+    """
+    Creation of UnitError, implemented when an exception must be raised in case of a wrong class type 
+    (TypeError) or a not supported value (ValueError) is passed according to the unit features.
+    """
+    
+    def __init__(self, unit=None, message="Class type or value not supported for unit to use."):
+        self.unit = unit
+        self.message = message
+        super().__init__(self.message)
+
+
+class SexagesimalError(Exception):
+    pass
