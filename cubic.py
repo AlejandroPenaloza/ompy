4561462db1147ae644eb54844d7710f63d320b46
Alejandro Penaloza <alejandro.penaloza.20@gmail.com>
2021-12-02 21:31:21 -0600

    docstrings, parameters and function roots update commit

--- cubic.py
+++ cubic.py
@@ -1,5 +1,6 @@
 import math
 import fractions
+import sympy as sp
 import re
 import inspect
 from collections import Counter
@@ -7,9 +8,10 @@ from .exceptions import check_types, check_vals
 
 
 def F(
-        num,
-        get="fraction"
+    num,
+    get="fraction"
 ):
+
     """
     Transforms a number (int or float) to a Fraction object or , by passing it as
     str first so it is a more accurate value.
@@ -28,22 +30,28 @@ def F(
     num_d = num.denominator
 
     if get == "numerator":
+
         return num_n
 
     elif get == "denominator":
+
         return num_d
 
     elif get == "asfloat":
+
         return num_n / num_d
 
     elif get == "asstr":
+
         return f"{num_n}/{num_d}"
 
     else:
+
         return num
 
 
 def cbrt(radicand):
+
     """
     Calculates the real cubic root -principal value- of rational representations
     of real numbers.
@@ -57,8 +65,9 @@ def cbrt(radicand):
     float; cubic root.
     """
 
-    radicand_type_exc_msg = "class type not supported; only int, float or fractions.Fraction object expected."
-    check_types(radicand, (int, float, fractions.Fraction), radicand_type_exc_msg)
+    global _radicand_type_exc_msg
+    _radicand_type_exc_msg = "class type not supported; int, float or fractions.Fraction object expected."
+    check_types(radicand, (int, float, fractions.Fraction), _radicand_type_exc_msg)
     radicand = F(radicand)
 
     if radicand < 0:
@@ -71,21 +80,22 @@ def cbrt(radicand):
 
 
 def to_depressed(
-        a,
-        b,
-        c,
-        d
+    coef_a,
+    coef_b,
+    coef_c,
+    coef_d
 ):
+
     """
     Transforms a general cubic equation to a depressed cubic equation.
     It takes general coefficients 'a', 'b', 'c', 'd' and returns depressed coefficients 'p', 'q'.
 
     Parameters
     ----------
-    a: int, float; required (it cannot be 0).
-    b: int, float; required.
-    c: int, float; required.
-    d: int, float; required.
+    coef_a: int, float; required (it cannot be 0).
+    cof_b: int, float; required.
+    coef_c: int, float; required.
+    coef_d: int, float; required.
 
     Returns
     -------
@@ -94,31 +104,31 @@ def to_depressed(
     [1]: coefficient 'q'.
     """
 
-    coefs_type_exc_msg = "Class type not supported; int, float or fractions.Fraction object expected."
+    for coef in (coef_a, coef_b, coef_c, coef_d):
+        check_types(coef, (int, float, fractions.Fraction), _coefs_type_exc_msg)
 
-    for coef in (a, b, c, d):
-        check_types(coef, (int, float, fractions.Fraction), coefs_type_exc_msg)
-
-    a, b, c, d = F(a), F(b), F(c), F(d)
+    a, b, c, d = F(coef_a), F(coef_b), F(coef_c), F(coef_d)
     coef_p = (c / a) - (b ** 2) / (3 * (a ** 2))
     coef_q = 2 * (b / (3 * a)) ** 3 - (b * c) / (3 * (a ** 2)) + d / a
     coef_p = round(F(coef_p, get="asfloat"), 12)
     coef_q = round(F(coef_q, get="asfloat"), 12)
+
     return coef_p, coef_q
 
 
 def cbdelta(
-        p,
-        q,
-        as_frac=False
+    coef_p,
+    coef_q,
+    as_frac=False
 ):
+
     """
     Calculates the cubic discriminant (delta) for a depressed cubic equation.
 
     Parameters
     ----------
-    p: int, float; required. Coefficient 'p' from depressed equation.
-    q: int, float; required. Coefficient 'q' from depressed equation.
+    coef_p: int, float; required. Coefficient 'p' from depressed equation.
+    coef_q: int, float; required. Coefficient 'q' from depressed equation.
     as_frac: bool (True or False); optional. Object type to be returned.
 
     Returns
@@ -127,15 +137,14 @@ def cbdelta(
     float if as_frac passed as False; fractions.Fraction object otherwise.
     """
 
-    coefs_type_exc_msg = "Class type not supported; int, float or fractions.Fraction object expected."
-    as_frac_type_exc_msg = "Bool expected; True if delta required as fractions.Fraction object or False for float."
+    _as_frac_type_exc_msg = "Bool expected; True if delta required as fractions.Fraction object or False for float."
 
-    for coef in (p, q):
-        check_types(coef, (int, float, fractions.Fraction), coefs_type_exc_msg)
+    for coef in (coef_p, coef_q):
+        check_types(coef, (int, float, fractions.Fraction), _coefs_type_exc_msg)
 
-    check_vals(as_frac, (True, False), as_frac_type_exc_msg)
+    check_vals(as_frac, (True, False), _as_frac_type_exc_msg)
 
-    p, q = F(p), F(q)
+    p, q = F(coef_p), F(coef_q)
     delta = (q ** 2) + ((4 * (p ** 3)) / 27)
 
     if as_frac:
@@ -148,16 +157,17 @@ def cbdelta(
 
 
 def depressed_roots(
-        p,
-        q
+    coef_p,
+    coef_q
 ):
+
     """
     For depressed, or reduced, cubic equations returns a tuple with its roots.
 
     Parameters
     ----------
-    p: int, float, fractions.Fraction object; required. Coefficient 'p' from depressed equation.
-    q: int, float, fractions.Fraction object; required. Coefficient 'q' from depressed equation.
+    coef_p: int, float, fractions.Fraction object; required. Coefficient 'p' from depressed equation.
+    coef_q: int, float, fractions.Fraction object; required. Coefficient 'q' from depressed equation.
 
     Returns
     -------
@@ -167,13 +177,12 @@ def depressed_roots(
         [2]: str; third root, represents a real or complex number.
     """
 
-    delta = cbdelta(p, q, True)
-    p, q = F(p), F(q)
+    delta = cbdelta(coef_p, coef_q, True)
+    p, q = F(coef_p), F(coef_q)
 
     if delta > 0:
         z1 = cbrt(0.5 * (-q + math.sqrt(delta))) + cbrt(0.5 * (-q - math.sqrt(delta)))
         imaginary_part = math.sqrt(3 * (z1 ** 2) + 4 * p) * 0.5
-        print(imaginary_part)
         imaginary_part = round(F(imaginary_part, "asfloat"), 11)
         z1 = round(F(z1, "asfloat"), 11)
         real_part = -0.5 * z1
@@ -197,17 +206,22 @@ def depressed_roots(
     return z1, z2, z3
 
 
-def roots(a, b, c, d):
+def roots(
+    coef_a,
+    coef_b,
+    coef_c,
+    coef_d
+):
 
     """
     Calculates cubic general equation roots.
 
     Parameters
     ----------
-    a: int, float, required (cannot be 0). Equation coefficient 'a'.
-    b: int, float; required. Equation coefficient 'b'.
-    c: int, float; required. Equation coefficient 'c'.
-    d: int, float; required. Equation coefficient 'd'.
+    coef_a: int, float, required (cannot be 0). Equation coefficient 'a'.
+    coef_b: int, float; required. Equation coefficient 'b'.
+    coef_c: int, float; required. Equation coefficient 'c'.
+    coef_d: int, float; required. Equation coefficient 'd'.
 
     Returns
     -------
@@ -217,31 +231,29 @@ def roots(a, b, c, d):
         [2]: str. Third root, represents a real or complex number.
     """
 
+    for coef in (coef_a, coef_b, coef_c, coef_d):
+        check_types(coef, (int, float, fractions.Fraction), _coefs_type_exc_msg)
 
-    a, b, c, d = float(a), float(b), float(c), float(d)
-    variable_change = -b/(3*a)
-    p = (c / a) - (b ** 2) / (3 * (a ** 2))
-    q = 2 * (b / (3 * a)) ** 3 - (b * c) / (3 * (a ** 2)) + d / a
-    cor_depressed_eq_roots = cb_depressed_roots(p, q)
-    delta = cbdelta(p, q)
+    a, b, c, d = F(coef_a), F(coef_b), F(coef_c), F(coef_d)
+    variable_change = -b / (3 * a)
+    corresp_p = (c / a) - (b ** 2) / (3 * (a ** 2))
+    corresp_q = 2 * (b / (3 * a)) ** 3 - (b * c) / (3 * (a ** 2)) + d / a
+    cor_depressed_eq_roots = depressed_roots(corresp_p, corresp_q)
+    delta = cbdelta(corresp_p, corresp_q)
 
-    if delta < 0:
-        x1 = str(round(float(cor_depressed_eq_roots[0]) + variable_change, 10))
-        x2 = str(round(float(cor_depressed_eq_roots[1]) + variable_change, 10))
-        x3 = str(round(float(cor_depressed_eq_roots[2]) + variable_change, 10))
-        return x1, x2, x3
+    if delta <= 0:
+        x1 = str(round(float(cor_depressed_eq_roots[0]) + variable_change, 11))
+        x2 = str(round(float(cor_depressed_eq_roots[1]) + variable_change, 11))
+        x3 = str(round(float(cor_depressed_eq_roots[2]) + variable_change, 11))
 
-    elif delta == 0:
-        x1 = str(round(float(cor_depressed_eq_roots[0]) + variable_change, 10))
-        x2 = str(round(float(cor_depressed_eq_roots[1]) + variable_change, 10))
-        x3 = str(round(float(cor_depressed_eq_roots[2]) + variable_change, 10))
         return x1, x2, x3
 
     else:
-        z1 = cbrt(0.5*(-q + math.sqrt(delta))) + cbrt(0.5*(-q - math.sqrt(delta)))
-        real_part = -0.5*z1
-        imaginary_part = 0.5*math.sqrt(3*(z1**2) + 4*p)
-        x1 = str(round(z1 + variable_change, 10))
-        x2 = str(round(real_part + variable_change, 10)) + " + " + str(round(imaginary_part, 10)) + "i"
-        x3 = str(round(real_part + variable_change, 10)) + " - " + str(round(imaginary_part, 10)) + "i"
+        z1 = cbrt(0.5 * (-corresp_q + F(math.sqrt(delta)))) + cbrt(0.5 * (-corresp_q - F(math.sqrt(delta))))
+        real_part = -0.5 * z1
+        imaginary_part = 0.5 * math.sqrt(3 * (z1 ** 2) + 4 * corresp_p)
+        x1 = str(round(z1 + variable_change, 11))
+        x2 = str(round(real_part + variable_change, 11)) + " + " + str(round(imaginary_part, 11)) + "i"
+        x3 = str(round(real_part + variable_change, 11)) + " - " + str(round(imaginary_part, 11)) + "i"
+
         return x1, x2, x3
